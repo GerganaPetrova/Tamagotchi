e3981b4db4e5033572fa227595e97cfd98674abb
GerganaPetrova <josephin.bella@gmail.com>
2014-05-27 12:07:13 +0300

    Add separate config file

--- core.py
+++ core.py
@@ -1,4 +1,5 @@
 from kivy import clock
+from config import *
 
 class Food:
 
@@ -10,49 +11,81 @@ class Food:
 class Tamagotchi:
 
     def __init__(self, name):
-        self.name = 5
-        self._energy = 5
-        self._happy = 5
-        self._health = 5
-        self._hunger = 5
-        self._clarity = 5
+        self._is_alive = True
+        self.name = name
+        self._energy = STARTING_ENERGY
+        self._play = STARTING_PLAY
+        self._health = STARTING_HEALTH
+        self._hunger = STARTING_HUNGER
+        self._clarity = STARTING_CLARITY
+        self._happiness = self._calculate_happiness()
 
-    def feed(self, food):
+
+    def _calculate_happiness(self):
+        return (self._energy + self._play + self._health
+                + self._hunger + self._clarity) / 5
+
+    def _feed(self, food):
         self.hunger += food.nutritive_value
-        if self.hunger > 10:
-            self.hunger = 10
+        if self.hunger > HUNGER:
+            self.hunger = HUNGER
 
-    def heal(self):
-        if self.heal < 5:
-            self.health += 2
+    def _heal(self):
+        if self.health < HEALTH / 2:
+            self.health += HEALTH_UPDATE
         else:
-            self.health = 10
+            self.health = HEALTH
 
-    def clean(self):
-        self.clarity += 2
+    def _clean(self):
+        self.clarity += CLARITY_UPDATE
 
+    def _kill(self):
+        self._is_alive = False
+
+    @property
     def is_hungry(self):
-        return self._hunger < 10
+        return self._hunger < HUNGER
 
+    @property
     def is_tired(self):
-        return self._energy < 10
+        return self._energy < ENERGY
 
+    @property
     def is_sad(self):
-        return self._happy < 10
+        return self._play < PLAY
 
+    @property
     def is_sick(self):
-        return self._health < 10
+        return self._health < HEALTH
 
+    @property
     def is_dirty(self):
-        return self._clarity < 10
+        return self._clarity < CLARITY
 
+    @property
+    def is_alice(self):
+        return self._is_alive
 
     def state(self):
         tamagotchi_state = {
-            'tired':  self.is_tired(),
-            'hungry': self.is_hungry(),
-            'sad':    self.is_sad(),
-            'sick':   self.is_sick(),
-            'dirty':  self.is_dirty(),
+            'tired':  self.is_tired,
+            'hungry': self.is_hungry,
+            'sad':    self.is_sad,
+            'sick':   self.is_sick,
+            'dirty':  self.is_dirty,
+            'alive':  self.is_alive,
         }
         return tamagotchi_state
+
+
+
+class Panda(Tamagotchi):
+
+    def __init__(self, name):
+        Tamagotchi.__init__(self, name)
+
+
+    def play(self):
+        pass
+
+
