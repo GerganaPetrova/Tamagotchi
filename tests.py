6baea308359f655e587c5900541b39683627bbaa
GerganaPetrova <josephin.bella@gmail.com>
2014-06-27 18:43:18 +0300

    Add more tests

--- tests.py
+++ tests.py
@@ -22,38 +22,41 @@ class TestTamagotchi(unittest.TestCase):
     def test_is_dirty(self):
         self.assertTrue(self.tamagotchi.is_dirty)
 
-    def test_clean(self):
-        self.tamagotchi.clean()
-        self.assertEqual(self.tamagotchi._clean, 7)
+    def test_clear(self):
+        self.tamagotchi.clear()
+        self.assertEqual(self.tamagotchi.clean, 7)
 
-    def test_full_clean(self):
-        self.tamagotchi.clean()
-        self.tamagotchi.clean()
-        self.tamagotchi.clean()
-        self.assertEqual(self.tamagotchi._clean, 10)
+    def test_full_clear(self):
+        self.tamagotchi.clear()
+        self.tamagotchi.clear()
+        self.tamagotchi.clear()
+        self.assertEqual(self.tamagotchi.clean, 10)
 
     def test_feed(self):
         self.tamagotchi.feed()
-        self.assertEqual(self.tamagotchi._hunger, 7)
+        self.assertEqual(self.tamagotchi.hunger, 7)
 
     def test_full_feed(self):
         self.tamagotchi.feed()
         self.tamagotchi.feed()
         self.tamagotchi.feed()
-        self.assertEqual(self.tamagotchi._hunger, 10)
+        self.assertEqual(self.tamagotchi.hunger, 10)
 
     def test_heal(self):
         self.tamagotchi.heal()
-        self.assertEqual(self.tamagotchi._health, 7)
+        self.assertEqual(self.tamagotchi.health, 7)
 
     def test_full_heal(self):
         self.tamagotchi.heal()
         self.tamagotchi.heal()
-        self.assertEqual(self.tamagotchi._health, 10)
+        self.assertEqual(self.tamagotchi.health, 10)
 
+    def test_sleep(self):
+        self.tamagotchi.sleep()
+        self.assertFalse(self.tamagotchi.is_tired)
 
     def test_calculate_strarting_happiness(self):
-        self.assertEqual(self.tamagotchi._happiness, self.tamagotchi.calculate_happiness())
+        self.assertEqual(self.tamagotchi.happiness, self.tamagotchi.calculate_happiness())
 
     def test_calculate_happiness(self):
         self.tamagotchi._energy = 10
@@ -74,13 +77,27 @@ class TestTamagotchi(unittest.TestCase):
         self.tamagotchi._energy = 2
         self.tamagotchi.get_sick()
 
-        self.assertEqual(self.tamagotchi._health, 3)
+        self.assertEqual(self.tamagotchi.health, 3)
 
 
     def test_kill(self):
         self.tamagotchi.kill()
         self.assertFalse(self.tamagotchi.is_alive)
 
+    def test_hunger_property(self):
+        self.assertEqual(self.tamagotchi.hunger, 5)
+
+    def test_energy_property(self):
+        self.assertEqual(self.tamagotchi.energy, 5)
+
+    def test_health_property(self):
+        self.assertEqual(self.tamagotchi.health, 5)
+
+    def test_happiness_property(self):
+        self.assertEqual(self.tamagotchi.happiness, 5)
+
+    def test_clean_property(self):
+        self.assertEqual(self.tamagotchi.clean, 5)
 
 if __name__ == '__main__':
     unittest.main()
